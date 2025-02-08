603105bff618c3431b86e094b6199fadcbaa24ba
Dávid Groniewsky <99834433+gro-david@users.noreply.github.com>
2025-02-08 17:51:29 +0100

    Bump version

--- launcher_backend.nix
+++ launcher_backend.nix
@@ -4,7 +4,7 @@ with lib;
 let 
 launcher = pkgs.stdenv.mkDerivation {
     pname = "launcher";
-    version = "1.0.0";
+    version = "1.1.0";
     src = pkgs.fetchFromGitHub {
     owner = "gro-david";
     repo = "launcher";
