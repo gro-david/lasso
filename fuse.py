f1775582f46524db5db98632742252fc5307009d
Dávid Groniewsky <gro.david.d@gmail.com>
2025-02-26 14:43:18 +0100

    Partially through enabling custom modes. A backup is needed.

--- fuse.py
+++ fuse.py
@@ -10,27 +10,28 @@ from modules import system
 from modules import read_conf as conf
 
 try:
-	# import hacks from the just added paths
-	sys.path.append(conf.hacks_path)
-	import hacks # type: ignore
+    # import hacks from the just added paths
+    sys.path.append(conf.hacks_path)
+    import hacks # type: ignore
 except Exception as e:
-	print("Could not import hacks. Please check the following error message, and open an issue if required.")
-	print(e)
+    print("Could not import hacks. Please check the following error message, and open an issue if required.")
+    print(e)
 
 # Function to get system info
 def get_system_info():
-	while True:
-		top_bar = ""
-		try:
-			top_bar = hacks.top_bar.get() # type: ignore
-		except Exception as e:
-			top_bar = hacks.get_fallback_top_bar() # type: ignore
+    while True:
+        top_bar = ""
+        try:
+            top_bar = hacks.top_bar.get() # type: ignore
+        except Exception as e:
+            top_bar = hacks.get_fallback_top_bar() # type: ignore
 
-		with open("/tmp/launcher_top_bar", "w") as f:
-			f.write(top_bar)
+        with open("/tmp/launcher_top_bar", "w") as f:
+            f.write(top_bar)
 
-		time.sleep(0.5)
+        time.sleep(0.5)
 
+# runs the fzf command with the correct status and options. finally returns the selected value
 def run_fzf(options):
     fzf_command = (
         "fzf --header-lines=0 --no-info "
@@ -41,7 +42,28 @@ def run_fzf(options):
     result = subprocess.run(fzf_command, input=fzf_input, text=True, shell=True, stdout=subprocess.PIPE)
     return result.stdout.strip()
 
+# adds the additional options for quiting and switching modes. also change modes/quit if requested, otherwise return the selected option
+def create_options(options):
+    index = list(modes.keys()).index(mode)
+    relevant_modes = list(modes.keys())
+    relevant_modes.pop(index)
+    if not ":q" in options:
+        options.append(":q")
+        options.extend(relevant_modes)
+
+
+    selection = run_fzf(options)
+
+    if selection == ":q": exit()
+    elif selection in modes:
+        modes[selection]()
+        return ""
+    return selection
+
+# the normal operating mode, for launching apps
 def normal_mode():
+    global mode
+    mode = ':n'
     desktop_dirs = conf.app_dirs
     desktop_files = [
         str(file) for dir_ in desktop_dirs for file in Path(dir_).glob("*.desktop") if file.is_file()
@@ -72,32 +94,27 @@ def normal_mode():
     options.extend(conf.app_names)
     exec_map.update(conf.app_exec_map)
 
-    if not ":q" in options: options.extend([":w", ":d", ":q"])
-    selection = run_fzf(options)
+    selection = create_options(options)
 
-    if selection == ":w":
-        window_mode()
-    elif selection == ":d":
-        dashboard_mode()
-    elif selection == ":q":
-        exit()
-    elif selection in exec_map:
-        try:
-            path = os.environ["PATH"]
-            if exec_map[selection]["env_path"] != "":
-                os.environ["PATH"] = exec_map[selection]["env_path"]
-            command = f"setsid {exec_map[selection]["exec"]} > /dev/null 2>&1 &"
-            os.environ["PATH"] = path
-            result = subprocess.run(command, shell=True, capture_output=True, text=True)
-            if result.returncode != 0:
-                print(f"Error launching {selection}:\n{result.stderr}")
-            else:
-                exit()
-        except Exception as e:
-            print(f"Failed to launch {selection}: {e}")
+    if not selection in exec_map: return
+    try:
+        path = os.environ["PATH"]
+        if exec_map[selection]["env_path"] != "":
+            os.environ["PATH"] = exec_map[selection]["env_path"]
+        command = f"setsid {exec_map[selection]["exec"]} > /dev/null 2>&1 &"
+        os.environ["PATH"] = path
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        if result.returncode != 0:
+            print(f"Error launching {selection}:\n{result.stderr}")
+        else:
+            exit()
+    except Exception as e:
+        print(f"Failed to launch {selection}: {e}")
 
 # Window mode: Focus a window
 def window_mode():
+    global mode
+    mode = ":w"
     window_data = subprocess.getoutput("niri msg windows")
     windows = window_data.split("\n\n")
 
@@ -116,41 +133,37 @@ def window_mode():
             options.append(title)
             window_map[title] = window_id
 
-    if not ":q" in options: options.extend([":n", ":d", ":q"])
-    selection = run_fzf(options)
+    selection = create_options(options)
+    if not selection in window_map: return
 
-    if selection == ":n":
-        normal_mode()
-    elif selection == ":d":
-        dashboard_mode()
-    elif selection == ":q":
-        exit()
-    elif selection in window_map:
-        window_id = window_map[selection]
-        subprocess.run(f"niri msg action focus-window --id {window_id}", shell=True)
-        exit()
+    window_id = window_map[selection]
+    subprocess.run(f"niri msg action focus-window --id {window_id}", shell=True)
+    exit()
 
 # Dashboard mode: Launch configured apps
 def dashboard_mode():
+    global mode
+    mode = ":d"
     options = conf.dashboard_names
     exec_map = conf.dashboard_exec_map
 
-    if not ":q" in options: options.extend([":n", ":w", ":q"])
-    selection = run_fzf(options)
+    selection = create_options(options)
+    if not selection in exec_map: return
+    path = os.environ["PATH"]
+    if exec_map[selection]["env_path"] != "":
+        os.environ["PATH"] = exec_map[selection]["env_path"]
+    subprocess.run(exec_map[selection]["exec"], shell=True)
+    os.environ["PATH"] = path
+    dashboard_mode()
 
-    if selection == ":n":
-        normal_mode()
-    elif selection == ":w":
-        window_mode()
-    elif selection == ":q":
-        exit()
-    elif selection in exec_map:
-        path = os.environ["PATH"]
-        if exec_map[selection]["env_path"] != "":
-            os.environ["PATH"] = exec_map[selection]["env_path"]
-        subprocess.run(exec_map[selection]["exec"], shell=True)
-        os.environ["PATH"] = path
-        dashboard_mode()
+# define the builtin modes and add the custom ones, a global variable for storing the current mode is also created
+modes = {
+    ":w": window_mode,
+    ":n": normal_mode,
+    ":d": dashboard_mode
+}
+modes.update(hacks.modes.modes) # type: ignore
+mode = ''
 
 # Start system info updater thread
 threading.Thread(target=get_system_info, daemon=True).start()
